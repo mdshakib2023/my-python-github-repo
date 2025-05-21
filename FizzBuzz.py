059808d62a2b2b3d4184cf73f6fbc427901f01b7
Md Shakib <158600908+mdshakib2023@users.noreply.github.com>
2025-05-21 17:36:15 +0600

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,13 @@
+#FizzBuzz
+
+for i in range(1,100):
+    if i % 3 == 0:
+        print("Fizz")
+    elif i % 5 ==0:
+        print("Buzz")
+    elif i % 3 == 0 and i % 5 == 0:
+            print("Fizz Buzz")    
+    else:
+        print(i)    
+    
+       
