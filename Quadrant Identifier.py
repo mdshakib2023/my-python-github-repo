b28b12c0bfb24237b66a1cb4db9fad41091ed9a3
Md Shakib <158600908+mdshakib2023@users.noreply.github.com>
2025-06-13 20:19:29 +0600

    Update Quadrant Identifier.py

--- Quadrant Identifier.py
+++ Quadrant Identifier.py
@@ -1,8 +1,19 @@
 #Quadrant Identifier
 
-x = float(input("Enter your Quadrant Identifier:"))
-y = float(input("Enter your Quadrant Identifier:"))
+x = float(input("enter the number of x:"))
+y = float(input("enter the numnber of y:"))
 
-if x == 0 and y == 0:
-    print("The first Quadrant Identifier")
-     
+if x > 0 and y > o :
+    print("first number is quadrant .")
+elif x < 0 and y > 0:
+    print("second number is quadrant.")
+elif x < 0 and y < 0:
+    print("third number is quadrant.")
+elif x > and y < 0 :
+    print("four number is quadrant.")
+elif x == 0 and y == 0:
+    print("the origin")
+elif x == 0:
+    print("the y-axis.")
+elif y == 0:
+    print("the x-axis.")
