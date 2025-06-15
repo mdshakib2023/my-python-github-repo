4444b50a44f2b9b59fd6c3a9bc858bc454833a84
Md Shakib <158600908+mdshakib2023@users.noreply.github.com>
2025-06-15 19:56:01 +0600

    Update Number Reverser.py

--- Number Reverser.py
+++ Number Reverser.py
@@ -1,5 +1,14 @@
-# number cheeker
+# number reverser 
 
-number = input("Enter number:")
-reversed_number = number[::-1]
-print("Reversed number:",reversed_number)
+num = int(input("enter your number:"))
+reverse = 0
+is_negative = num < 0
+num = abs(num)
+while num!= 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num //= 10
+if is_negative:
+    reverse = -reverse
+print("reverse number:",reverse)
+
